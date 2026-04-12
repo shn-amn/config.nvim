cf518ed0acb487127fd1c8fc2846f2c786065c4d
Shahin <zero@shn.one>
2026-04-12 22:45:06 +0200

    multiple terminal sessions

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -14,7 +14,14 @@ return {
           winblend = 0,
         },
         start_in_insert = true,
+        on_create = function(term)
+          local cmd = 'export NVIM_TERM_ID=' .. term.id .. ' && clear\13'
+          vim.api.nvim_chan_send(term.job_id, cmd)
+        end,
       }
+      vim.keymap.set('n', '<leader>t1', '<cmd>1ToggleTerm<cr>', { desc = '[T]erminal [1]' })
+      vim.keymap.set('n', '<leader>t2', '<cmd>2ToggleTerm<cr>', { desc = '[T]erminal [2]' })
+      vim.keymap.set('n', '<leader>t3', '<cmd>3ToggleTerm<cr>', { desc = '[T]erminal [3]' })
     end,
   },
 }
