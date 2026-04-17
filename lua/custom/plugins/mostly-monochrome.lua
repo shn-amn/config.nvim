1cfd1c634b470a72f89d9bac59682f1a848ee79e
shn <shahin.amini@opensee.io>
2026-04-17 17:20:02 +0200

    remote mostly-monochrom

--- lua/custom/plugins/mostly-monochrome.lua
+++ lua/custom/plugins/mostly-monochrome.lua
@@ -1,11 +1,10 @@
 ---@module 'lazy'
 ---@type LazySpec
 return {
-  dir = '~/Code/mostly-monochrome',
+  'shn-amn/mostly-monochrome',
   lazy = false,
   priority = 1000,
-  dev = true,
-  dependencies = { 'rktjmp/lush.nvim' }, -- The manager handles this
+  dependencies = { 'rktjmp/lush.nvim' },
   config = function()
     vim.cmd 'colorscheme mostly-monochrome'
     vim.api.nvim_create_autocmd('VimEnter', {
